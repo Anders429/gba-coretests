4b6f110329f88ad61ae9fe778ab157ee43b5fd31
Anders429 <anders.stefan.evensen@gmail.com>
2024-12-27 13:34:39 -0700

    Enable test returning Result now that gba_test supports it.

--- src/iter/traits/iterator.rs
+++ src/iter/traits/iterator.rs
@@ -310,19 +310,19 @@ fn test_try_find() {
     }
 }
 
-// #[test]
-// fn test_try_find_api_usability() -> Result<(), Box<dyn std::error::Error>> {
-//     let a = ["1", "2"];
+#[test]
+fn test_try_find_api_usability() -> Result<(), Box<dyn core::error::Error>> {
+    let a = ["1", "2"];
 
-//     let is_my_num = |s: &str, search: i32| -> Result<bool, std::num::ParseIntError> {
-//         Ok(s.parse::<i32>()? == search)
-//     };
+    let is_my_num = |s: &str, search: i32| -> Result<bool, core::num::ParseIntError> {
+        Ok(s.parse::<i32>()? == search)
+    };
 
-//     let val = a.iter().try_find(|&&s| is_my_num(s, 2))?;
-//     assert_eq!(val, Some(&"2"));
+    let val = a.iter().try_find(|&&s| is_my_num(s, 2))?;
+    assert_eq!(val, Some(&"2"));
 
-//     Ok(())
-// }
+    Ok(())
+}
 
 #[test]
 fn test_position() {
